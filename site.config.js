3dad81ee4e29227b2bc910c7a2e8060301a9feb9
Rulo <andres@vitau.mx>
2024-02-10 07:17:37 -0500

    feat(site): config site

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "rulocode",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I develop everything ",
+    email: "andres@rulocode.com",
+    linkedin: "rulocode",
+    github: "rulocode",
     instagram: "",
   },
   projects: [
